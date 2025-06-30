f5e24614319bff0d3aa5b468aab0ef5719fdadfd
Anilsahu07 <myhousehome1111@gmail.com>
2025-06-30 21:32:40 +0530

    changes

--- frontend/src/pages/BookedEvents.jsx
+++ frontend/src/pages/BookedEvents.jsx
@@ -15,7 +15,7 @@ const BookedEvents = () => {
     
   return (
     <div className='w-screen lg:flex lg:items-center lg:justify-center justify-center'>
-        {bookedEvents.length===0?<p>No events booked yet!!</p>: bookedEvents.map(e=>(
+        {bookedEvents.length===0?<p className='text-center'>No events booked yet!!</p>: bookedEvents.map(e=>(
             <div key={e._id} className='flex flex-col gap-2 items-center justify-around w-full p-3'>
               <div className='lg:flex lg:flex-col lg:items-center lg:justify-center text-center lg:w-[500px] gap-2 p-3 bg-purple-500 rounded-xl font-[montserrat]'>
                 <img className='lg:w-full w-full rounded-xl' src={e.photo} alt="404" />
--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -5,12 +5,12 @@ const Profile = () => {
     
   return (
     <div className='w-screen h-[92vh] flex justify-center items-center flex-wrap p-3 lg:bg-white bg-black'>
+      <h1>User's Profile</h1>
       <div className='w-fit flex flex-col items-center gap-3 text-center '>
         <img className='lg:w-56 lg:h-56 w-52 h-52 rounded-full outline lg:outline-red-400 outline-blue-500 lg:object-cover object-cover' src={apps.user.image} alt="" />
         <h1 className='text-2xl font-normal text-pink-500 mt-3 font-[poppins]'>{apps.user.username}</h1>
         <h1 className='text-4xl font-[montserrat] font-semibold lg:text-black text-white'>{apps.user.email}</h1>
-        <h1 className='text-xl font-[montserrat] text-blue-600 mt-2'>{apps.user.role}</h1>
-        <h1 className='text-2xl'>{"*".repeat(apps.user.password.length)}</h1>
+        <h1 className='text-3xl font-[montserrat] text-blue-600 mt-2'>{apps.user.role}</h1>
       </div>
     </div>
   )
