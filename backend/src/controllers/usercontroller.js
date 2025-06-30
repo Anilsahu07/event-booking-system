bf70426fe5ea1db70f17eb6e33df4cd617575d9c
Anilsahu07 <myhousehome1111@gmail.com>
2025-06-30 22:31:14 +0530

    changes

--- backend/src/controllers/usercontroller.js
+++ backend/src/controllers/usercontroller.js
@@ -14,7 +14,7 @@ module.exports.userRegister= async(req,res)=>{
             username,
             password:hashPassword,
             email,
-            role:"user"
+            role
         })
         
 
