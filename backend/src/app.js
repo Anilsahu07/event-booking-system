8ef350166a2244f26abaadb9d7da1b99fffa187c
Anilsahu07 <myhousehome1111@gmail.com>
2025-06-30 22:10:29 +0530

    changes

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,7 +8,7 @@ const cookieParser= require("cookie-parser")
 
 app.use(express.json())
 app.use(cors({
-     origin: "https://splendorous-custard-9e1051.netlify.app",
+     origin: "https://curious-alfajores-539000.netlify.app",
      credentials: true
 }))
 app.use(cookieParser())
