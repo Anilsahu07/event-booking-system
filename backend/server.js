7721b1df32b78cbab4906c45ab79eb51f9ef693b
Anilsahu07 <myhousehome1111@gmail.com>
2025-06-30 12:47:43 +0530

    changes in backend folder

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 const app= require("./src/app")
 const connect= require("./src/db/db")
-connect()
 
+const dotenv= require("dotenv")
+dotenv.config();
+
+connect()
 
 app.listen(3000, ()=>{
     console.log("Server is running on the port 3000");
--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -2,7 +2,10 @@ const mongoose= require("mongoose")
 
 
 const connect=()=>{
-    mongoose.connect("mongodb://localhost:27017/event-booking").then(()=>{
+    mongoose.connect(process.env.MONGO_URI,{
+         useNewUrlParser: true,
+        useUnifiedTopology: true
+    }).then(()=>{
         console.log("Database connected");
         
     }).catch((error)=>{
